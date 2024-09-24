428709d1a8ae0e111f6a72f585c1cefed89629d5
Jovani Reyes <jovanireyes82@gmail.com>
2024-09-23 18:57:02 -0600

    Added Mailing Functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,38 @@
 import './FormStyles.css';
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
 const Form = () => {
-  return <div className="form">
-    <form>
+  const form = useRef();
+  const sendMail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(process.env.REACT_APP_EMAILJS_SS, process.env.REACT_APP_EMAILJS_TD, form.current, process.env.REACT_APP_EMAILJS_PK)
+      .then((result) => {
+        console.log(result.text);
+        alert('Message sent successfully!');
+      }, (error) => {
+        console.log(error.text);
+        alert('Failed to send message. Please try again.');
+      });
+  };
+
+  return (
+  <div className="form">
+    <form ref={form} onSubmit={sendMail}>
       <label>Name</label>
-      <input type="text"></input>
+      <input type="text" name="name" required/>
 
       <label>Email</label>
-      <input type="text"></input>
+      <input type="email" name="email" required/>
 
       <label>Subject</label>
-      <input type="text"></input>
+      <input type="text" name="subject" required/>
       
       <label>Message</label>
-      <textarea rows="6" placeholder='Type message here'></textarea>
-      <button className="btn"> Submit</button>
-    </form>
-  </div>
+      <textarea name="message" rows="6" placeholder='Type message here' required></textarea>
+      <button className="btn" type="submit"> Submit</button>
+      </form>
+    </div>
+  );
 };
 export default Form;
